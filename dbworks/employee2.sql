8323707fdac6cf1cc62f799233b303ce81a9abd9
dlatnrud <dlatnrud2268@naver.com>
2021-09-24 11:34:17 +0900

    dbworks

---
+++ dbworks/employee2.sql
@@ -0,0 +1,21 @@
+SELECT * FROM employee;
+-- ROLLBACK - COMMIT 전에 실행하면 이전 상태로 돌아감 --
+ROLLBACK;
+
+
+DELETE FROM employee WHERE emp_id = 'e1002';
+
+-- 검색 --
+SELECT * FROM employee WHERE emp_id = 'e10002';
+SELECT emp_id, name, FROM employee WHERE emp_id = 'e1003';
+
+-- 사원 추가 (INSERT INTO ~ VALUES) --
+INSERT INTO employee(emp_id, name) VALUES ('e1004', '안산');
+
+-- 사원 정보 수정 (UPDATE 테이블 이름 SET WHERE) --
+UPDATE employee SET age = 31 WHERE emp_id = 'e1002';
+
+-- 사원 삭제 (DELETE FROM 테이블 이름 WHERE) --
+-- 테이블 유지 - 테이터만 삭제 --
+
+COMMIT;
